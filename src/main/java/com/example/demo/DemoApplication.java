e0b29df3d793453fba854de40497aa1cdfd292e5
wywangxiaowai <wy314163@163.com>
2020-12-05 19:49:47 +0800

    修改启动类

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		System.out.println("启动中");
+		System.out.println("启动中");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
